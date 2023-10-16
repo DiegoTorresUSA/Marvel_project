84e6c47a7633e31c34a3a99637d9d7f6ebdd2dbe
DiegoT <mando2567@gmail.com>
2023-10-16 03:05:23 -0500

    ADD landing page

--- js/app.js
+++ js/app.js
@@ -1,57 +1,51 @@
-document.addEventListener("DOMContentLoaded", () =>{
-   getHeroes(10)
-    // getHeroesp(20)
-});
+/* Llamado del modal */
+const openModal = document.getElementById("user-join")
+const closeModal = document.querySelector("#user-close")
+console.log(closeModal)
+const modal = document.querySelector("#modal")
+console.log(modal)
+openModal.addEventListener("click", ()=>{
+    modal.showModal();
+    modal.classList.add("modal--show")
+})
 
-/* ----DATOS API ver https://developer.marvel.com/docs
-const ts = 1;
-const pubKey = '1e4f4e96c273cfae3a27fb936b6a1a5f';
-const hash= 'bbc5a1123f3bd1ce184d8f1a9691e6e7'
+closeModal.addEventListener("click", ()=>{
+    modal.close()
+    modal.classList.add("modal--close")
+})
+/* Llamado del modal */
 
-const [timestamp, publicKey, h] = [ts, pubKey, hash];
 
-const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${h}&limit=2'
-https://gateway.marvel.com/v1/public/characters?ts=1&apikey=1e4f4e96c273cfae3a27fb936b6a1a5f&hash=bbc5a1123f3bd1ce184d8f1a9691e6e7&limit=2
-https://gateway.marvel.com/v1/public/characters?ts=1&apikey=1e4f4e96c273cfae3a27fb936b6a1a5f&hash=bbc5a1123f3bd1ce184d8f1a9691e6e7&limit=2
-*/
 
-const flex = document.querySelector('#card')
-let contentHTML = '';
+/******Control botones registro - ingreso*/
 
-async function fetchMarvelAPI(url){
-    const response = await fetch(url);
-    return await response.json()
+const btnSignin = document.getElementById('btn-signin')
+const btnSignup = document.getElementById('btn-signup')
+const nameField = document.getElementById('name-field')
+const nombres = document.getElementById('nombres')
+const contrasenia = document.getElementById('contrasenia')
+const title = document.getElementById('title')
+
+btnSignin.onclick = function (){
+    nameField.style.display = 'block';
+    nombres.style.display = 'none';
+    contrasenia.style.display = 'none';
+    title.innerHTML = 'SIGN IN'
+    btnSignin.classList.remove('disable')
+    btnSignup.classList.add('disable')
+}
+
+btnSignup.onclick = function (){
+    nameField.style.display = 'block';
+    nameField.classList.add('#nombres')
+    nombres.style.display = 'block';
+    title.innerHTML = 'REGISTRATE';
+    btnSignin.classList.add('disable');
+    btnSignup.classList.remove('disable');
+    console.log(nameField);
 }
 
 
 
-// const getHeroesp = async (limit) =>{
-//     for (let i = 0; i < limit; i++) {
-//         await fetch(urlAPI)
-//             .then((response) => response.json())
-//             .then(data => console.log(data));
-//     }
-// }
-const getHeroes = async (limit) =>{
-        await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=${limit}&apikey=1e4f4e96c273cfae3a27fb936b6a1a5f`)
-            .then((response) => response.json())
-            .then(data => {
-               for (const hero of data.data.results){
-                   let urlHero = hero.urls[0].url;
-                   // console.log(urlHero);
-                   contentHTML += `<article class="card">
-                                    <img src="/images/bg-pattern-card.svg" alt="imagen header card" class="card-header">
-                                    <div class="card-body">
-                                        <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="card-body-img">
-                                        <h1 class="card-body-title">
-                                            ${hero.name}
-                                        </h1>
-                                        <a href="${urlHero}" target="_blank" class="card-body-text"><span>Comics</span></a>
+/******Control botones registro - ingreso*/
 
-                                    </div>
-                                </article>
-                                `
-               }
-               flex.innerHTML = contentHTML
-            });
-}
